c88e8a92e39635ea212f5b47325768d0ee867bb7
t-naganuma <takuya.naganuma@gmail.com>
2025-06-07 16:51:34 +0900

    fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -51,6 +51,7 @@ const config: Config = {
 					DEFAULT: 'hsl(var(--destructive))',
 					foreground: 'hsl(var(--destructive-foreground))',
 				},
+				income: 'hsl(var(--income))',
 				border: 'hsl(var(--border))',
 				input: 'hsl(var(--input))',
 				ring: 'hsl(var(--ring))',
